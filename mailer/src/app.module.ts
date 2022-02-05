5a163a9bb28eaace3c1db5a29bc1988b576c9b28
Juni Brosas <powerlogic1992@gmail.com>
2022-02-05 22:39:16 +0800

    add config service

--- mailer/src/app.module.ts
+++ mailer/src/app.module.ts
@@ -1,9 +1,23 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { MailgunModule } from '@nextnm/nestjs-mailgun';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MailgunModule.forAsyncRoot({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          username: configService.get('EMAIL_USERNAME'),
+          key: configService.get('EMAIL_API_KEY'),
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
