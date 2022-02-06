9625ff8831ab8831b109f2251d2695ad4602494c
Juni Brosas <powerlogic1992@gmail.com>
2022-02-07 00:00:48 +0800

    create basic sending of email message

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Create a proof-of-concept for a communication between services using NestJS fram
 Run `npm install` to all services.
 Run `npm run start:dev` to all services.
 
+You will need redis installed on your server to support the queue. The configuration of this database is simple.
+
 References:
 https://dzone.com/articles/how-to-create-a-nestjs-redis-microservice
 
--- mailer/src/app.processor.ts
+++ mailer/src/app.processor.ts
@@ -1,5 +1,11 @@
-import { Processor, Process } from '@nestjs/bull';
-import { Logger } from '@nestjs/common';
+import {
+  Processor,
+  Process,
+  OnQueueActive,
+  OnQueueCompleted,
+  OnQueueFailed,
+} from '@nestjs/bull';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MailgunService } from '@nextnm/nestjs-mailgun';
 import { Job } from 'bull';
@@ -9,6 +15,7 @@ interface IConfirmRegistrationJob {
   recipient: string;
 }
 
+@Injectable()
 @Processor(MAIL_QUEUE)
 export class MailProcessor {
   private readonly _logger = new Logger(MailProcessor.name);
@@ -18,10 +25,50 @@ export class MailProcessor {
     private readonly _configService: ConfigService,
   ) {}
 
+  @OnQueueActive()
+  public onActive(job: Job) {
+    this._logger.debug(`Processing job ${job.id} of type ${job.name}`);
+  }
+
+  @OnQueueCompleted()
+  public onComplete(job: Job) {
+    this._logger.debug(`Completed job ${job.id} of type ${job.name}`);
+  }
+
+  @OnQueueFailed()
+  public onError(job: Job<any>, error: any) {
+    this._logger.error(
+      `Failed job ${job.id} of type ${job.name}: ${error.message}`,
+      error.stack,
+    );
+  }
+
   @Process(CONFIRM_REGISTRATION)
   public async confirmRegistration(job: Job<IConfirmRegistrationJob>) {
     this._logger.log(
-      `Sending confirmation registration email to ${job.data.recipient}`,
+      `Sending confirm registration email to '${job.data.recipient}'`,
     );
+
+    try {
+      const data = {
+        from: 'Excited User <me@samples.mailgun.org>',
+        to:
+          this._configService.get('EMAIL_AUTHORIZED_RECIPIENT') ||
+          job.data.recipient,
+        subject: 'Attack on titan',
+        text: 'Rumbling, rumbling, rumbling, its coming!',
+      };
+
+      const message = await this._mailgunService.createEmail(
+        this._configService.get('EMAIL_DOMAIN'),
+        data,
+      );
+
+      this._logger.log(message);
+    } catch (error) {
+      this._logger.error(
+        `Failed to send confirmation email to '${job.data.recipient}'`,
+      );
+    }
   }
 }
